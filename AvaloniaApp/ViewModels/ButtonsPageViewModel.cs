85d247844213ccf521884a4c64a5892487301f42
Stuart Hallows <shallows@somarkinnovations.com>
2024-07-07 19:29:18 +0930

    Sidebar

---
+++ AvaloniaApp/ViewModels/ButtonsPageViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace AvaloniaApp.ViewModels;
+
+public class ButtonsPageViewModel: ViewModelBase
+{
+    
+}
--- AvaloniaApp/ViewModels/MainWindowViewModel.cs
+++ AvaloniaApp/ViewModels/MainWindowViewModel.cs
@@ -1,16 +1,60 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 namespace AvaloniaApp.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    [ObservableProperty]
-    private string _textBlockName = "Welcome to Avalonia!";
+    [ObservableProperty] private bool _isPaneOpen = true;
+
+    [ObservableProperty] private ViewModelBase _currentPage = new HomePageViewModel();
+
+    [ObservableProperty] private ListItemTemplate? _selectedListItem;
+
+    partial void OnSelectedListItemChanged(ListItemTemplate? value)
+    {
+        if (value is null)
+            return;
+        
+        var instance = Activator.CreateInstance(value.ModelType);
+        if (instance is null)
+        {
+            return;
+            
+        }
+
+        CurrentPage = (ViewModelBase)instance;
+    }
+
+    public ObservableCollection<ListItemTemplate> Items { get; } = new()
+    {
+        new ListItemTemplate(typeof(HomePageViewModel), "HomeRegular"),
+        new ListItemTemplate(typeof(ButtonsPageViewModel), "CursorHoverRegular")
+    };
     
     [RelayCommand]
-    private void ButtonOnClick()
+    private void TriggerPane()
     {
-        TextBlockName = "Hello, Avalonia!";
+        IsPaneOpen = !IsPaneOpen;
     }
+}
+
+public class ListItemTemplate
+{
+    public ListItemTemplate(Type type, string iconKey)
+    {
+        ModelType = type;
+        Label = type.Name.Replace("PageViewModel", string.Empty);
+        Application.Current!.TryFindResource(iconKey, out var res);
+        ListItemIcon = (StreamGeometry)res!;
+    }
+    
+    public string Label { get; set; }
+    public Type ModelType { get; set; }
+    public StreamGeometry ListItemIcon { get; set; }
 }
