f51bddcabab51be5f5e1515c3aaafe6714fc0dbe
Stuart Hallows <shallows@somarkinnovations.com>
2024-07-07 17:11:38 +0930

    Project setup

--- AvaloniaApp/Views/MainWindow.axaml.cs
+++ AvaloniaApp/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace AvaloniaApp.Views;
 
@@ -7,5 +9,11 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("Button clicked!");
+        TextBlock1.Text = "Button clicked!";
     }
 }
