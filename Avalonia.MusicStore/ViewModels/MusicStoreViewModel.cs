210f8a4f85579ad720d965c6b9cef12d270560d6
Stuart Hallows <stuart.hallows@gmail.com>
2024-07-10 21:52:05 +0930

    Mock search

--- Avalonia.MusicStore/ViewModels/MusicStoreViewModel.cs
+++ Avalonia.MusicStore/ViewModels/MusicStoreViewModel.cs
@@ -1,6 +1,39 @@
-﻿namespace Avalonia.MusicStore.ViewModels;
+﻿using System.Collections.ObjectModel;
+using ReactiveUI;
 
-public class MusicStoreViewModel
+namespace Avalonia.MusicStore.ViewModels;
+
+public class MusicStoreViewModel : ViewModelBase
 {
+    private string? _searchText;
+    private bool _isBusy;
+    private AlbumViewModel? _selectedAlbum;
+
+    public MusicStoreViewModel()
+    {
+        SearchResults.Add(new AlbumViewModel());
+        SearchResults.Add(new AlbumViewModel());
+        SearchResults.Add(new AlbumViewModel());
+    }
     
+    public string? SearchText
+    {
+        get => _searchText;
+        set => this.RaiseAndSetIfChanged(ref _searchText, value);
+    }
+
+    public bool IsBusy
+    {
+        get => _isBusy;
+        set => this.RaiseAndSetIfChanged(ref _isBusy, value);
+    }
+    
+
+    public ObservableCollection<AlbumViewModel> SearchResults { get; } = new();
+
+    public AlbumViewModel? SelectedAlbum
+    {
+        get => _selectedAlbum;
+        set => this.RaiseAndSetIfChanged(ref _selectedAlbum, value);
+    }
 }
